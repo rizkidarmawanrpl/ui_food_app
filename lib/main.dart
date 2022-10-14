87c06829d77d29105516005fafff285ed50b4fb1
Rizki Darmawan <rizkidarmawanrpl@gmail.com>
2022-10-14 18:14:57 +0700

    Commit first

---
+++ lib/main.dart
@@ -0,0 +1,261 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_polygon_clipper/flutter_polygon_clipper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data =
+      await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'BelajarFlutter',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+// IMAGES
+var meatImage = 'https://i.ibb.co/5FRwHtB/unnamed.jpg';
+var foodImage = 'https://i.ibb.co/qk6PzDy/London-broil.jpg';
+var burgerImage =
+    'https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+var chickenImage =
+    'https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+
+// COLORS
+var textYellow = const Color(0xFFf6c24d);
+var iconYellow = const Color(0xFFf4bf47);
+var green = const Color(0xFF4caf6a);
+var greenLight = const Color(0xFFd8ebde);
+var red = const Color(0xFFf36169);
+var redLight = const Color(0xFFf2dcdf);
+var blue = const Color(0xFF398bcf);
+var blueLight = const Color(0xFFc1dbee);
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: const MyActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: const MyAppbar(),
+      body: Container(
+        child: ListView(
+          children: const <Widget>[
+            // SizedBox(height: 16.0),
+            // MyAppBar(),
+            SizedBox(height: 16.0),
+            FoodListview(),
+            SizedBox(height: 16.0),
+            // SelectTypeSection(),
+            // SizedBox(height: 16.0),
+            // MenuItemList()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyActionButton extends StatelessWidget {
+  const MyActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      child: FlutterClipPolygon(
+        sides: 6,
+        borderRadius: 0.0, // Defaults to 0.0 degrees
+        rotate: 0.0, // Defaults to 0.0 degrees
+        boxShadows: [
+          PolygonBoxShadow(color: Colors.red, elevation: 1.0),
+          PolygonBoxShadow(color: Colors.grey, elevation: 5.0)
+        ],
+        child: Container(
+          color: iconYellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Icon(FontAwesomeIcons.book),
+              SizedBox(height: 4.0),
+              Text(
+                'Menu',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyAppbar extends StatelessWidget {
+  const MyAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        height: 60.0,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: const <Widget>[
+                  Icon(Icons.home),
+                  Text('Home', style: TextStyle(fontSize: 12.0))
+                ],
+              ),
+              Column(
+                children: const <Widget>[
+                  Icon(Icons.search, color: Colors.black45),
+                  Text('Search', style: TextStyle(fontSize: 12.0))
+                ],
+              ),
+              Container(
+                width: 100.0,
+              ),
+              Column(
+                children: const <Widget>[
+                  Icon(Icons.shop, color: Colors.black45),
+                  Text(
+                    'Wishlist',
+                    style: TextStyle(fontSize: 12.0),
+                  )
+                ],
+              ),
+              Column(
+                children: const <Widget>[
+                  Icon(
+                    Icons.shopping_cart,
+                    color: Colors.black45,
+                  ),
+                  Text('Cart', style: TextStyle(fontSize: 12.0))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FoodListview extends StatelessWidget {
+  const FoodListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Container(
+        height: 160.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: const <Widget>[
+            ItemCard(),
+            ItemCard(),
+            ItemCard(),
+            ItemCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Container(
+        height: 160.0,
+        width: 300.0,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(meatImage), fit: BoxFit.cover),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 160.0,
+              width: 300.0,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.black.withOpacity(0.1), Colors.black],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Spacer(),
+                  Text(
+                    '25% OFF',
+                    style: TextStyle(
+                      color: textYellow,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                      letterSpacing: 1.1,
+                    ),
+                  ),
+                  const Text(
+                    'ON FIRST 3 ORDERS',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      letterSpacing: 1.1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SelectTypeSection extends StatelessWidget {
+  const SelectTypeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: SingleChildScrollView(
+        
+      ),
+    );
+  }
+}
